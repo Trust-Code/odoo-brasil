67d917f6fb3ab2cb4cbdd459d1bbacd613cad09a
Danimar Ribeiro <danimaribeiro@gmail.com>
2016-05-01 19:55:10 -0300

    New modules structure

---
+++ l10n_br_website_sale/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Website Sale Brazil',
+    'summary': """Module do adapt Odoo website_sale to Brazil""",
+    'version': '1.0',
+    'category': 'website',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>'
+    ],
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+    ],
+}
