4895883fea195b3ec05c10563d74e119cc39faf8
martini97 <alessandrofmartini@gmail.com>
2016-09-14 17:21:41 -0300

    Não cria linha DSR na folha de pagamentos se não houver faltas

--- br_hr_payroll/model/hr_payslip.py
+++ br_hr_payroll/model/hr_payslip.py
@@ -24,31 +24,42 @@ class HrPayslip(models.Model):
     def get_worked_day_lines(self, contract_ids, date_from, date_to):
         res = super(HrPayslip, self).get_worked_day_lines(
             contract_ids, date_from, date_to)
-        leaves = self.env['hr.holidays'].search(
-            [('employee_id', '=', self.employee_id.id),
-             ('date_from', '>=', date_from),
-             ('date_to', '<=', date_to),
-             ('state', '=', 'validate'),
-             ('type', '=', 'remove')])
-        dsr = {}
-        for leave in leaves:
-            # BUG: Fazer validação da falta
-            if leave.holiday_status_id.name == 'Unpaid':
-                leave_start = fields.Datetime.from_string(leave.date_from)
-                dsr[self.week_of_month(leave_start)] = leave_start
-        res += [{
+        employee = self.employee_id
+        if self.get_contract(employee, date_from, date_to):
+            leaves = self.env['hr.holidays'].search(
+                [('employee_id', '=', self.employee_id.id),
+                 ('date_from', '>=', date_from),
+                 ('date_to', '<=', date_to),
+                 ('state', '=', 'validate'),
+                 ('type', '=', 'remove')])
+            dsr = {}
+            for leave in leaves:
+                # BUG: Fazer validação da falta
+                if leave.holiday_status_id.name == 'Unpaid':
+                    leave_start = fields.Datetime.from_string(leave.date_from)
+                    dsr[self.week_of_month(leave_start)] = leave_start
+            dsr_dict = {
              'name': "Descanso Semanal Remunerado",
              'sequence': 8,
              'code': 'DSR',
              'number_of_days': len(dsr),
              'contract_id': self.contract_id,
-        },
-                {
-                     'name': "Provisão de #13",
-                     'sequence': 13,
-                     'code': 'P13',
-                     'number_of_days': 30,
-                     'contract_id': self.contract_id,
+             }
+            if dsr_dict['number_of_days'] != 0:
+                res += [dsr_dict, {
+                 'name': "Provisão de #13",
+                 'sequence': 13,
+                 'code': 'P13',
+                 'number_of_days': 30,
+                 'contract_id': self.contract_id,
+                }]
+            else:
+                res += [{
+                 'name': "Provisão de #13",
+                 'sequence': 13,
+                 'code': 'P13',
+                 'number_of_days': 30,
+                 'contract_id': self.contract_id,
                 }]
 
-        return res
+            return res
