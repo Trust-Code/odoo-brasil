39f94e997200e0f43f1076fc8245efadf2951b66
martini97 <alessandrofmartini@gmail.com>
2016-09-21 16:12:58 -0300

    Adicionado div de botões no xml do account.invoice ao br_base

--- br_base/__manifest__.py
+++ br_base/__manifest__.py
@@ -20,6 +20,7 @@
         'views/res_country_view.xml',
         'views/res_partner_view.xml',
         'views/res_company_view.xml',
+        'views/account_invoice.xml',
         'security/ir.model.access.csv',
     ],
     'test': [
