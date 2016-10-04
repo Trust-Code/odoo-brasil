378af91283e0e3f4b0a002aef76469f4694fa8db
Danimar Ribeiro <danimaribeiro@gmail.com>
2016-10-04 11:01:32 -0300

    Teste unitario para pedido de venda com frete, seguro e outros

--- br_sale_stock/tests/__init__.py
+++ br_sale_stock/tests/__init__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2016 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-
-# TODO Criar testes
+from . import br_sale_stock
