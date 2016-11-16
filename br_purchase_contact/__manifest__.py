20bc601c68da02792b02163d9a24004fa9d4b3ec
martini97 <alessandrofmartini@gmail.com>
2016-11-16 17:07:03 -0200

    Novos modulos de Contato de Venda/Compra

---
+++ br_purchase_contact/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2016  Alessandro Fernandes Martini - TrustCode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Contato de Compra',
+    'description': '',
+    'license': 'AGPL-3',
+    'author': 'TrustCode',
+    'website': 'http://trustcode.com.br/',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'br_sale',
+    ],
+    'data': [
+        'views/purchase_order.xml',
+    ],
+    'demo': [
+    ],
+    'category': 'Localisation',
+    'auto_install': True,
+
+}
