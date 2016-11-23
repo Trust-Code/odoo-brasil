1e77ab2b67d5eb9f9d6a3457cad1c0f2b398a5bd
martini97 <alessandrofmartini@gmail.com>
2016-11-23 15:46:59 -0200

    Adicionado campo de valor total e situção ao payment_order e inserido campo de data de emissão de cnab à view

--- br_boleto/models/payment_order.py
+++ br_boleto/models/payment_order.py
@@ -2,11 +2,17 @@
 # © 2016 Alessandro Fernandes Martini, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PaymentOrderLine(models.Model):
     _name = 'payment.order.line'
+
+    @api.multi
+    def _compute_state(self):
+        self.state = 'open'
+        if self.move_id.reconciled:
+            self.state = 'payed'
 
     name = fields.Char(string="Ref.", size=20)
     payment_order_id = fields.Many2one(
@@ -22,10 +28,20 @@ class PaymentOrderLine(models.Model):
         'payment.mode', string="Modo de pagamento")
     date_maturity = fields.Date(string="Vencimento")
     value = fields.Float(string="Valor", digits=(18, 2))
+    state = fields.Selection([("open", "Aberto"), ("payed", "Pago")],
+                             string="Situação",
+                             compute="_compute_state")
 
 
 class PaymentOrder(models.Model):
     _name = 'payment.order'
+
+    @api.depends('line_ids')
+    def _compute_amount_total(self):
+        amount_total = 0
+        for line in self.line_ids:
+            amount_total += line.value
+        self.amount_total = amount_total
 
     name = fields.Char(max_length=30, string="Nome", required=True)
     user_id = fields.Many2one('res.users', string='Responsável',
@@ -38,3 +54,5 @@ class PaymentOrder(models.Model):
     line_ids = fields.One2many('payment.order.line', 'payment_order_id',
                                required=True, string=u'Linhas de Cobrança')
     currency_id = fields.Many2one('res.currency', string='Moeda')
+    amount_total = fields.Float(string="Total",
+                                compute='_compute_amount_total')
