d564ffb6ada4aeba30db83b26f130a08b5e73aea
Michell Stuttgart <michellstut@gmail.com>
2016-11-28 12:10:19 -0200

    [FIX] Ajuste no metodo zip_search de br_crm_zip

--- br_crm_zip/models/crm_lead.py
+++ br_crm_zip/models/crm_lead.py
@@ -3,8 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, api
-from odoo.tools.translate import _
-from odoo.exceptions import Warning
 
 
 class CrmLead(models.Model):
@@ -14,37 +12,10 @@ class CrmLead(models.Model):
     @api.multi
     def zip_search(self):
         self.ensure_one()
-        obj_zip = self.env['br.zip']
-
-        zip_ids = obj_zip.zip_search_multi(
-            country_id=self.country_id.id,
-            state_id=self.state_id.id,
-            city_id=self.city_id.id,
-            district=self.district,
-            street=self.street,
-            zip_code=self.zip,
-        )
-
-        if len(zip_ids) == 1:
-            result = obj_zip.set_result(zip_ids[0])
-            self.write(result)
-            return True
-        else:
-            if len(zip_ids) > 1:
-                obj_zip_result = self.env['br.zip.result']
-                zip_ids = obj_zip_result.map_to_zip_result(
-                    zip_ids, self._name, self.id)
-
-                return obj_zip.create_wizard(
-                    self._name,
-                    self.id,
-                    country_id=self.country_id.id,
-                    state_id=self.state_id.id,
-                    city_id=self.city_id.id,
-                    district=self.district,
-                    street=self.street,
-                    zip_code=self.zip,
-                    zip_ids=[z.id for z in zip_ids],
-                )
-            else:
-                raise Warning(_('Nenhum registro encontrado'))
+        res = self.env['br.zip'].zip_search(country_id=self.country_id.id,
+                                            state_id=self.state_id.id,
+                                            city_id=self.city_id.id,
+                                            district=self.district,
+                                            street=self.street,
+                                            zip_code=self.zip)
+        self.update(res)
