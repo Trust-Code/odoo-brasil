6a1b9cc504347d6ae79c40e6cecdc497bb38e4e0
Danimar Ribeiro <danimaribeiro@gmail.com>
2016-12-02 15:04:19 -0200

    Exportação da nfse de florianopolis usando o parceiro comercial

--- wizard/nfse_export_invoice.py
+++ wizard/nfse_export_invoice.py
@@ -26,18 +26,19 @@ class NfseExportInvoice(models.TransientModel):
 
     def _invoice_vals(self, inv):
         tomador = {
-            'cnpj_cpf': re.sub('[^0-9]', '', inv.partner_id.cnpj_cpf or ''),
+            'cnpj_cpf': re.sub(
+                '[^0-9]', '', inv.commercial_partner_id.cnpj_cpf or ''),
             'inscricao_municipal': re.sub(
-                '[^0-9]', '', inv.partner_id.inscr_mun or '0000000'),
-            'name': inv.partner_id.legal_name,
-            'street': inv.partner_id.street,
-            'number': inv.partner_id.number,
-            'district': inv.partner_id.district,
-            'zip': re.sub('[^0-9]', '', inv.partner_id.zip or ''),
+                '[^0-9]', '', inv.commercial_partner_id.inscr_mun or '0000000'),
+            'name': inv.commercial_partner_id.legal_name,
+            'street': inv.commercial_partner_id.street,
+            'number': inv.commercial_partner_id.number,
+            'district': inv.commercial_partner_id.district,
+            'zip': re.sub('[^0-9]', '', inv.commercial_partner_id.zip or ''),
             'city_code': '%s%s' % (
-                inv.partner_id.state_id.ibge_code,
-                inv.partner_id.city_id.ibge_code),
-            'uf_code': inv.partner_id.state_id.code,
+                inv.commercial_partner_id.state_id.ibge_code,
+                inv.commercial_partner_id.city_id.ibge_code),
+            'uf_code': inv.commercial_partner_id.state_id.code,
             'email': inv.partner_id.email,
             'phone': re.sub('[^0-9]', '', inv.partner_id.phone or ''),
         }
@@ -55,9 +56,9 @@ class NfseExportInvoice(models.TransientModel):
             })
         emissao = fields.Date.from_string(inv.date_invoice)
         cfps = '9201'
-        if inv.company_id.city_id.id != inv.partner_id.city_id.id:
+        if inv.company_id.city_id.id != inv.commercial_partner_id.city_id.id:
             cfps = '9202'
-        if inv.company_id.state_id.id != inv.partner_id.state_id.id:
+        if inv.company_id.state_id.id != inv.commercial_partner_id.state_id.id:
             cfps = '9203'
         return {
             'tomador': tomador,
@@ -80,7 +81,7 @@ class NfseExportInvoice(models.TransientModel):
         xml = xml.replace('&', '&amp;')
         return {'name': u'{0}_{1}_nfse.xml'.format(
             invoice.number.replace('/', '-'),
-            re.sub('[^A-Za-z]', '', invoice.partner_id.name)),
+            re.sub('[^A-Za-z]', '', invoice.commercial_partner_id.name)),
             'content': xml}
 
     def _save_zip(self, xmls):
@@ -109,21 +110,21 @@ class NfseExportInvoice(models.TransientModel):
         xmls = []
         for invoice in invoice_ids:
             errors = []
-            if invoice.partner_id.is_company and\
-                    not invoice.partner_id.legal_name:
+            if invoice.commercial_partner_id.is_company and\
+                    not invoice.commercial_partner_id.legal_name:
                 errors += ['Razão Social incompleta.']
             if not invoice.partner_id.phone and invoice.partner_id.mobile:
                 invoice.partner_id.phone = invoice.partner_id.mobile
             if not invoice.partner_id.phone and not invoice.\
                     partner_id.mobile:
                 errors += ['Telefone incompleto.']
-            if not invoice.partner_id.city_id:
+            if not invoice.commercial_partner_id.city_id:
                 errors += ['Munícipio incompleto.']
-            if not invoice.partner_id.zip:
+            if not invoice.commercial_partner_id.zip:
                 errors += ['CEP incompleto.']
-            if not invoice.partner_id.cnpj_cpf:
+            if not invoice.commercial_partner_id.cnpj_cpf:
                 errors += ['CPF / CNPJ incompleto.']
-            if not invoice.partner_id.street:
+            if not invoice.commercial_partner_id.street:
                 errors += ['Logradouro incompleto.']
             if not invoice.partner_id.email:
                 errors += ['Email incompleto.']
