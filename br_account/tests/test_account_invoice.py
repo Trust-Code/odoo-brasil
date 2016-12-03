78514d49c01821a55acca63c023fae543dbfc3e5
Danimar Ribeiro <danimaribeiro@gmail.com>
2016-12-03 09:40:32 -0200

    Unit tests for ISSQN, II, and ICMS and ICMS reduced

--- br_account/tests/test_account_invoice.py
+++ br_account/tests/test_account_invoice.py
@@ -71,7 +71,7 @@ class TestAccountInvoice(TestBaseBr):
             # Verifico as linhas recebiveis
             self.assertEquals(len(invoice.receivable_move_line_ids), 1)
 
-    def test_invoice_simple_taxes(self):
+    def test_invoice_pis_cofins_taxes(self):
         for invoice in self.invoices:
 
             first_item = invoice.invoice_line_ids[0]
@@ -109,3 +109,112 @@ class TestAccountInvoice(TestBaseBr):
             self.assertEquals(invoice.cofins_base, 650.0)
             self.assertEquals(invoice.pis_value, 32.5)
             self.assertEquals(invoice.cofins_value, 97.5)
+
+            # Valido a fatura
+            invoice.action_invoice_open()
+
+            # Ainda deve ter os mesmos valores
+            self.assertEquals(invoice.pis_base, 650.0)
+            self.assertEquals(invoice.cofins_base, 650.0)
+            self.assertEquals(invoice.pis_value, 32.5)
+            self.assertEquals(invoice.cofins_value, 97.5)
+
+    def test_invoice_issqn_and_ii_taxes(self):
+        for invoice in self.invoices:
+
+            prod_item = invoice.invoice_line_ids[0]
+            serv_item = invoice.invoice_line_ids[1]
+
+            # II
+            prod_item.tax_ii_id = self.ii_6000
+            prod_item._onchange_tax_ii_id()
+            self.assertEquals(prod_item.price_total, 150.0)
+            self.assertEquals(prod_item.ii_base_calculo, 150.0)
+            self.assertEquals(prod_item.ii_valor, 90.0)
+            self.assertEquals(prod_item.ii_aliquota, 60.0)
+
+            # ISSQN
+            serv_item.tax_issqn_id = self.issqn_500
+            serv_item._onchange_tax_issqn_id()
+            self.assertEquals(serv_item.price_total, 500.0)
+            self.assertEquals(serv_item.issqn_base_calculo, 500.0)
+            self.assertEquals(serv_item.issqn_valor, 25.0)
+            self.assertEquals(serv_item.issqn_aliquota, 5.0)
+
+            # Totais
+            self.assertEquals(invoice.issqn_base, 500.0)
+            self.assertEquals(invoice.ii_value, 90.0)
+            self.assertEquals(invoice.issqn_value, 25.0)
+
+            # Valido a fatura
+            invoice.action_invoice_open()
+
+            # Ainda deve ter os mesmos valores
+            self.assertEquals(invoice.issqn_base, 500.0)
+            self.assertEquals(invoice.ii_value, 90.0)
+            self.assertEquals(invoice.issqn_value, 25.0)
+
+    def test_invoice_icms_normal_tax(self):
+        for invoice in self.invoices:
+
+            first_item = invoice.invoice_line_ids[0]
+
+            # ICMS
+            first_item.tax_icms_id = self.icms_1700
+            first_item._onchange_tax_icms_id()
+            self.assertEquals(first_item.price_total, 150.0)
+            self.assertEquals(first_item.icms_base_calculo, 150.0)
+            self.assertEquals(first_item.icms_valor, 25.5)
+            self.assertEquals(first_item.icms_aliquota, 17.0)
+
+            for item in invoice.invoice_line_ids:
+                item.tax_icms_id = self.icms_1700
+                item._onchange_tax_icms_id()
+                self.assertEquals(item.icms_base_calculo, item.price_total)
+                self.assertEquals(
+                    item.icms_valor, round(item.price_total * 0.17, 2))
+                self.assertEquals(item.icms_aliquota, 17.0)
+
+            self.assertEquals(invoice.icms_base, 650.0)
+            self.assertEquals(invoice.icms_value, 110.5)
+
+            # Valido a fatura
+            invoice.action_invoice_open()
+
+            # Ainda deve ter os mesmos valores
+            self.assertEquals(invoice.icms_base, 650.0)
+            self.assertEquals(invoice.icms_value, 110.5)
+
+    def test_invoice_icms_reducao_base_tax(self):
+        for invoice in self.invoices:
+
+            first_item = invoice.invoice_line_ids[0]
+
+            # ICMS com Redução de base
+            first_item.tax_icms_id = self.icms_1700
+            first_item.icms_aliquota_reducao_base = 10.0
+            first_item._onchange_tax_icms_id()
+            self.assertEquals(first_item.price_total, 150.0)
+            self.assertEquals(first_item.icms_base_calculo, 135.0)
+            self.assertEquals(first_item.icms_valor, 22.95)
+            self.assertEquals(first_item.icms_aliquota, 17.0)
+
+            for item in invoice.invoice_line_ids:
+                item.tax_icms_id = self.icms_1700
+                item.icms_aliquota_reducao_base = 10.0
+                item._onchange_tax_icms_id()
+                self.assertEquals(
+                    item.icms_base_calculo, round(item.price_total * 0.9, 2))
+                self.assertEquals(
+                    item.icms_valor, round(item.price_total * 0.9 * 0.17, 2))
+                self.assertEquals(item.icms_aliquota, 17.0)
+
+            self.assertEquals(invoice.icms_base, 585.0)
+            self.assertEquals(invoice.icms_value, 99.45)
+
+            # Valido a fatura
+            invoice.action_invoice_open()
+
+            # Ainda deve ter os mesmos valores
+            self.assertEquals(invoice.icms_base, 585.0)
+            self.assertEquals(invoice.icms_value, 99.45)
