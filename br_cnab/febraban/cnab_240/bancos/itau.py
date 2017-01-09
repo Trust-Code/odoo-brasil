01eec0d3a5d9e1208663e56c32335c71d081bdde
Carlos R. Silveira <crsilveira@gmail.com>
2017-01-09 12:56:29 -0200

    Calculando o digito do nosso número.

--- br_cnab/febraban/cnab_240/bancos/itau.py
+++ br_cnab/febraban/cnab_240/bancos/itau.py
@@ -54,4 +54,41 @@ class Itau240(Cnab240):
         :return:
         """
         vals = super(Itau240, self)._prepare_segmento(line)
+        dv = self.dv_nosso_numero(
+            line.payment_mode_id.bank_account_id.bra_number,
+            line.payment_mode_id.bank_account_id.acc_number,
+            line.payment_mode_id.boleto_carteira,
+            line.nosso_numero
+        )
+        vals['nosso_numero_dv'] = dv
         return vals
+
+    def dv_nosso_numero(self, agencia, conta, carteira, nosso_numero):
+        composto = "%4s%5s%3s%8s" % (agencia.zfill(4), conta.zfill(5),
+                                     carteira.zfill(3), nosso_numero.zfill(8))
+        return self.modulo10(composto)
+
+    @staticmethod
+    def modulo10(num):
+        if not isinstance(num, basestring):
+            raise TypeError
+        soma = 0
+        peso = 2
+        for c in reversed(num):
+            parcial = int(c) * peso
+            if parcial > 9:
+                s = str(parcial)
+                parcial = int(s[0]) + int(s[1])
+            soma += parcial
+            if peso == 2:
+                peso = 1
+            else:
+                peso = 2
+
+        resto10 = soma % 10
+        if resto10 == 0:
+            modulo10 = 0
+        else:
+            modulo10 = 10 - resto10
+
+        return modulo10
