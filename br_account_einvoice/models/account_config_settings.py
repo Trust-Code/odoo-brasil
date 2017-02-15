2dbfcd3a77a69680ff52691e06b4d3568488be57
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-02-15 09:14:11 -0200

    Mudando para o einvoice o código base de envio Cada módulo precisa apenas sobrescrever _find_attachments para adicionar anexos

--- br_account_einvoice/models/account_config_settings.py
+++ br_account_einvoice/models/account_config_settings.py
@@ -8,8 +8,9 @@ from odoo import api, fields, models
 class AccountConfigSettings(models.TransientModel):
     _inherit = 'account.config.settings'
 
-    nfe_email_template = fields.Many2one('mail.template',
-                                         string="Template de Email para NFe")
+    nfe_email_template = fields.Many2one(
+        'mail.template', string="Template de Email para NFe",
+        domain=[('model_id.model', '=', 'account.invoice')])
 
     def get_default_nfe_email_template(self, fields):
         mail = self.env['mail.template'].search(
