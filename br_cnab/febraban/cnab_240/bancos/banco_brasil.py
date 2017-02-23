3f6e46c243c418c74fc565b76d5b911705d5ff7a
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-02-23 15:49:19 -0300

    Correção na impressão do boleto santander

--- br_cnab/febraban/cnab_240/bancos/banco_brasil.py
+++ br_cnab/febraban/cnab_240/bancos/banco_brasil.py
@@ -14,6 +14,8 @@ class BancoBrasil240(Cnab240):
 
     def _prepare_header(self):
         vals = super(BancoBrasil240, self)._prepare_header()
+        vals['codigo_convenio_banco'] = self.format_codigo_convenio_banco(
+            self.order.payment_mode_id)
         vals['controlecob_numero'] = self.order.id
         vals['controlecob_data_gravacao'] = self.data_hoje()
         return vals
@@ -27,7 +29,8 @@ class BancoBrasil240(Cnab240):
             parcela = line.name.split('/')[1]
         except:
             parcela = line.name
-        vals['codigo_convenio_banco'] = self.format_codigo_convenio_banco(line)
+        vals['codigo_convenio_banco'] = self.format_codigo_convenio_banco(
+            line.payment_mode_id)
         vals['carteira_numero'] = int(line.payment_mode_id.boleto_carteira[:2])
         vals['nosso_numero'] = self.format_nosso_numero(
             nossonumero, digito, parcela,
@@ -47,8 +50,8 @@ class BancoBrasil240(Cnab240):
         return "%s%s%s%s4     " % (nosso_numero.zfill(9), digito,
                                    parcela.zfill(2), modalidade)
 
-    def format_codigo_convenio_banco(self, line):
-        num_convenio = line.payment_mode_id.bank_account_id.codigo_convenio
-        carteira = line.payment_mode_id.boleto_carteira[:2]
-        boleto = line.payment_mode_id.boleto_variacao.zfill(3)
+    def format_codigo_convenio_banco(self, payment_mode):
+        num_convenio = payment_mode.bank_account_id.codigo_convenio
+        carteira = payment_mode.boleto_carteira[:2]
+        boleto = payment_mode.boleto_variacao.zfill(3)
         return "%s0014%s%s  " % (num_convenio, carteira, boleto)
