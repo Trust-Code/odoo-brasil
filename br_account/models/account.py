ec50a55562b8c00177088e571e5d819cf858b1b2
Michell Stuttgart <michellstut@gmail.com>
2017-04-06 18:08:38 -0300

    [NEW] Adicionado campo 'code_first_digit' em 'account.account'
    Campo utilizado para armazenar o primeiro digito do código. Utilizado para fins de filtro

--- br_account/models/account.py
+++ br_account/models/account.py
@@ -2,7 +2,7 @@
 # © 2016 Danimar Ribeiro, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountPaymentTerm(models.Model):
@@ -11,3 +11,17 @@ class AccountPaymentTerm(models.Model):
     indPag = fields.Selection(
         [('0', u'Pagamento à Vista'), ('1', u'Pagamento à Prazo'),
          ('2', 'Outros')], 'Indicador de Pagamento', default='1')
+
+
+class AccountAccount(models.Model):
+    _inherit = 'account.account'
+
+    code_first_digit = fields.Char(compute='_compute_code_first_digit',
+                                   string=u'Primeiro Dígito',
+                                   store=True)
+
+    @api.multi
+    @api.depends('code')
+    def _compute_code_first_digit(self):
+        for rec in self:
+            rec.code_first_digit = rec.code[0] if rec.code else ''
