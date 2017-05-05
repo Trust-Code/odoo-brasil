5b01ff68280c93ad7a8835c1a084129a2768ee12
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-05-05 14:21:49 -0300

    Enviando a data da multa zerado considera a data de vencimento para o calculo de juros

--- br_cnab/febraban/cnab_240/bancos/santander.py
+++ br_cnab/febraban/cnab_240/bancos/santander.py
@@ -35,6 +35,8 @@ class Santander240(Cnab240):
         vals['codigo_juros'] = int(vals['codigo_juros'])
         vals['juros_mora_taxa'] = vals['juros_mora_taxa'] * Decimal('1000')
         vals['juros_multa'] = vals['juros_multa'] * Decimal('1000')
+        # Data da multa fica zerado, dai ele considera data de vencimento
+        vals['data_multa'] = 0
         # tipo documento : 1- Tradicional , 2- Escritural
         vals['tipo_documento'] = 1
         especie = 2
