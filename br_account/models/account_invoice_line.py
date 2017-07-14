8111e534192e7af71f73bada83b5507d3a66406d
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-07-14 11:29:57 -0300

    Remoção do campo ICMS próprio o mesmo já existia com outro nome Ajuste na nomenclatura do campo Mensagens no cancelamento da NFE

--- br_account/models/account_invoice_line.py
+++ br_account/models/account_invoice_line.py
@@ -29,7 +29,6 @@ class AccountInvoiceLine(models.Model):
             'incluir_ipi_base': self.incluir_ipi_base,
             'icms_st_aliquota_mva': self.icms_st_aliquota_mva,
             'icms_aliquota_reducao_base': self.icms_aliquota_reducao_base,
-            'icms_aliquota_proprio': self.icms_aliquota_proprio,
             'icms_st_aliquota_reducao_base':
             self.icms_st_aliquota_reducao_base,
             'icms_st_aliquota_deducao': self.icms_st_aliquota_deducao,
@@ -53,7 +52,7 @@ class AccountInvoiceLine(models.Model):
                  'incluir_ipi_base', 'tem_difal', 'icms_aliquota_reducao_base',
                  'ipi_reducao_bc', 'icms_st_aliquota_mva', 'tax_simples_id',
                  'icms_st_aliquota_reducao_base', 'icms_aliquota_credito',
-                 'icms_st_aliquota_deducao', 'icms_aliquota_proprio')
+                 'icms_st_aliquota_deducao')
     def _compute_price(self):
         currency = self.invoice_id and self.invoice_id.currency_id or None
         price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
@@ -244,9 +243,6 @@ class AccountInvoiceLine(models.Model):
     icms_st_base_calculo = fields.Float(
         'Base ICMS ST', required=True, compute='_compute_price', store=True,
         digits=dp.get_precision('Account'), default=0.00)
-    icms_aliquota_proprio = fields.Float(
-        '% ICMS Próprio', digits=dp.get_precision('Discount'),
-        default=0.00, help="Usado em casos onde existe apenas ST sem ICMS")
     icms_st_aliquota = fields.Float(
         '% ICMS ST', digits=dp.get_precision('Discount'),
         default=0.00)
@@ -296,9 +292,10 @@ class AccountInvoiceLine(models.Model):
     icms_valor_credito = fields.Float(
         u"Valor de Crédito", compute='_compute_price', store=True)
     icms_st_aliquota_deducao = fields.Float(
-        string=u"% Dedução", help="Alíquota interna ou interestadual aplicada \
+        string=u"% ICMS Próprio",
+        help="Alíquota interna ou interestadual aplicada \
          sobre o valor da operação para deduzir do ICMS ST - Para empresas \
-         do Simples Nacional")
+         do Simples Nacional ou usado em casos onde existe apenas ST sem ICMS")
 
     # =========================================================================
     # ISSQN
