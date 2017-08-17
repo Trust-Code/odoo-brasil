b4094b0b27afd6fc2ed03e4778a4162d86e93317
Mackilem <mack.vdl@gmail.com>
2017-08-17 19:17:46 -0300

    somente insere um smart button para facilitar o acesso quando pagamento manual

--- br_account_payment/models/account_payment.py
+++ br_account_payment/models/account_payment.py
@@ -10,6 +10,8 @@ class AccountPayment(models.Model):
 
     move_line_id = fields.Many2one('account.move.line',
                                    string="Linha de fatura")
+    count_payments = fields.Integer('Linha(s)',
+        compute = 'compute_count_payments')
 
     @api.model
     def default_get(self, fields):
@@ -22,3 +24,27 @@ class AccountPayment(models.Model):
     def _create_payment_entry(self, amount):
         self = self.with_context(move_line_to_reconcile=self.move_line_id)
         return super(AccountPayment, self)._create_payment_entry(amount)
+
+    @api.depends('partner_id', 'partner_type')
+    def compute_count_payments(self):
+        if self.partner_type == 'supplier':
+            account_type = 'payable'
+        else:
+            account_type = 'receivable'
+
+        self.count_payments = self.env['account.move.line'].search_count(
+            [('partner_id', '=', self.partner_id.id),
+            ('user_type_id.type', '=', account_type),
+            ('amount_residual', '!=', 0)])
+
+    @api.multi
+    def action_view_account_payment(self):
+        if self.partner_type == 'supplier':
+            action_ref = 'br_account_payment.action_payable_move_lines'
+        else:
+            action_ref = 'br_account_payment.action_receivable_move_line'
+
+        action = self.env.ref(action_ref).read()[0]
+        action['context'] = {'search_default_partner_id': self.partner_id.id}
+
+        return action
