99659d8b509c62e86ea3435c9b155cd1887d145c
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-09-04 22:54:45 -0300

    Implementando ação de envio de email com boletos

---
+++ br_boleto/models/res_company.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# © 2017 Danimar Ribeiro <danimaribeiro@gmail.com>, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    boleto_email_tmpl = fields.Many2one(
+        'mail.template', string="Template de Email para Boleto")
