abd2eab4add8bace139ee9ec818e427bfc87398f
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-10-09 01:45:50 -0300

    Criado regiões, lista de preços automática por região Região pode ser de cidades ou estados

---
+++ br_product/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2017 Danimar Ribeiro <danimaribeiro@gmail.com>, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{  # pylint: disable=C8101,C8103
+    'name': 'Brazilian Localization for Product',
+    'summary': """Brazilian Localization for Product""",
+    'description': """Brazilian Localization for Product""",
+    'version': '11.0.1.0.0',
+    'category': 'Localization',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+    ],
+    'depends': [
+        'product', 'br_base',
+    ],
+    'data': [
+        'views/product_pricelist.xml',
+    ],
+}
