c222fdb7e815256c8bb6172d7bb94371408dc8b9
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-10-09 00:36:57 -0300

    Módulo para localizar account voucher, filtrando contas corretas no recibo Nova classificação na conta contabil Junção de contas a receber e pagar

---
+++ br_account_voucher/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2017 Danimar Ribeiro <danimaribeiro@gmail.com>, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{  # pylint: disable=C8101,C8103
+    'name': 'Brazilian Localization for Voucher',
+    'summary': """Brazilian Localization for Voucher""",
+    'description': """Brazilian Localization for Voucher""",
+    'version': '11.0.1.0.0',
+    'category': 'Localization',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+    ],
+    'depends': [
+        'account_voucher'
+    ],
+    'data': [
+        'views/account_voucher.xml',
+    ],
+}
