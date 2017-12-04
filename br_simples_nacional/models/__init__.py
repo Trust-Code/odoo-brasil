ae9eb41e59f050ea622cec64b40f6af3faf828a2
Felipe <paloschi.eca@gmail.com>
2017-12-04 17:45:29 -0200

    ajustes na view e adicionado contributors no manifest

---
+++ br_simples_nacional/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# © 2017 Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import account
+from . import res_company
+from . import simples_nacional
