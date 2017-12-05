8324347b7ed7297e0c6419964684ec044cec6470
Felipe <paloschi.eca@gmail.com>
2017-12-05 13:30:48 -0200

    transferidos métodos para res_company.py, deletado account.py

--- br_simples_nacional/__manifest__.py
+++ br_simples_nacional/__manifest__.py
@@ -3,13 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-{
+{  # pylint: disable=C8101,C8103
     'name': "Simples Nacional",
 
     'summary': """
         Calcular a alíquota efetiva do Simples Nacional""",
 
     'description': """
+        Método que passa a ser obigatório para o calculo da
+        alíquota do simples nacional, com base na receita bruta
+        dos últimos 12 meses da empresa.
     """,
     'author': "Trustcode",
     'website': "http://www.trustcode.com.br",
--- br_simples_nacional/models/__init__.py
+++ br_simples_nacional/models/__init__.py
@@ -2,6 +2,5 @@
 # © 2017 Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import account
 from . import res_company
 from . import simples_nacional
