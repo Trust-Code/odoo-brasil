20a1377ad9f59176fbf0535b79e280ae750f7f58
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-02-05 18:08:05 -0200

    Criação de campo para salvar o número AEDF - Floripa

--- br_nfse_florianopolis/models/res_company.py
+++ br_nfse_florianopolis/models/res_company.py
@@ -8,6 +8,8 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
+    aedf = fields.Char(string="Número AEDF", size=10,
+                       help="Número de autorização para emissão de NFSe")
     client_id = fields.Char(string='Client Id', size=50)
     client_secret = fields.Char(string='Client Secret', size=50)
     user_password = fields.Char(string='Senha Acesso', size=50)
