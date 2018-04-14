57db7b4db7107d05eb85e7c8e041ee9eb32ebdac
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-04-13 21:22:57 -0300

    WIP - Implementando nota de BH

---
+++ br_nfse_bh/models/__init__.py
@@ -0,0 +1,7 @@
+# © 2018 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import account_invoice
+from . import invoice_eletronic
+from . import br_account_service
+from . import res_company
