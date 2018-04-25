d4401fada97ad9e46765c8347f799d4ff0fe1157
Felipe <paloschi.eca@gmail.com>
2018-04-25 14:53:09 -0300

    ajustes redirect user notification

--- br_base/models/res_users.py
+++ br_base/models/res_users.py
@@ -22,31 +22,35 @@ class ResUsers(models.Model):
         compute='_compute_channel_names')
 
     @api.multi
-    def notify_info(self, message, title=None, sticky=False):
+    def notify_info(self, message, title=None, sticky=False, redirect=False):
         title = title or _('Information')
         self._notify_channel(
-            'notify_info_channel_name', message, title, sticky)
+            'notify_info_channel_name', message, title, sticky, redirect)
 
     @api.multi
-    def notify_warning(self, message, title=None, sticky=False):
+    def notify_warning(self, message, title=None, sticky=False,
+                       redirect=False):
         title = title or _('Warning')
         self._notify_channel(
-            'notify_warning_channel_name', message, title, sticky)
+            'notify_warning_channel_name', message, title, sticky, redirect)
 
     @api.multi
-    def _notify_channel(self, channel_name_field, message, title, sticky):
+    def _notify_channel(self, channel_name_field, message, title, sticky,
+                        redirect):
         bus_message = {
             'message': message,
             'title': title,
-            'sticky': sticky
+            'sticky': sticky,
+            'redirect': redirect
         }
         notifications = [(getattr(record, channel_name_field), bus_message)
                          for record in self]
         self.env['bus.bus'].sendmany(notifications)
 
     @api.multi
-    def notify(self, message, type=None, title=None, sticky=None):
-        if type == 'warning':
-            self.notify_warning(message, title, sticky)
+    def notify(self, message, title=None, sticky=None, redirect=False,
+               warning=False):
+        if warning:
+            self.notify_warning(message, title, sticky, redirect)
         else:
-            self.notify_info(message, title, sticky)
+            self.notify_info(message, title, sticky, redirect)
