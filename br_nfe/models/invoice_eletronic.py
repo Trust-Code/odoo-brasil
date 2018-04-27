4aa8cb5273cbd659f4e613b05e86d011f731564f
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-04-27 11:46:10 -0300

    Realiza o ajuste de hora no envio da carta de correção

--- br_nfe/models/invoice_eletronic.py
+++ br_nfe/models/invoice_eletronic.py
@@ -905,8 +905,7 @@ src="/report/barcode/Code128/' + self.chave_nfe + '" />'
                 'tpAmb': 2 if self.ambiente == 'homologacao' else 1,
                 'CNPJ': re.sub('[^0-9]', '', self.company_id.cnpj_cpf),
                 'chNFe': self.chave_nfe,
-                'dhEvento': dt_evento.strftime(
-                    '%Y-%m-%dT%H:%M:%S-03:00'),
+                'dhEvento': dt_evento.strftime('%Y-%m-%dT%H:%M:%S-03:00'),
                 'nSeqEvento': self.sequencial_evento,
                 'nProt': self.protocolo_nfe,
                 'xJust': justificativa
