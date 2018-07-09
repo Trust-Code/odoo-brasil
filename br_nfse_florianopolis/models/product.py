a1f80f7c43761dec1347514a82748a03671f9091
carcaroff <johnychenjy@gmail.com>
2018-07-09 17:08:18 -0300

    [11.0]CNAE proprio para cada serviço

---
+++ br_nfse_florianopolis/models/product.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# © 2018 Johny Chen Jy, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    service_cnae = fields.Many2one(
+        'br_account.cnae', string="CNAE do Serviço")
