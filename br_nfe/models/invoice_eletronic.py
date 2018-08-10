b0ed6f6a3f8b2c207b9221ded79d42c65ccfbd0f
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-08-10 14:18:10 -0300

    [FIX] Zera o valor de pagamento quando não possuir duplicatas

--- br_nfe/models/invoice_eletronic.py
+++ br_nfe/models/invoice_eletronic.py
@@ -632,7 +632,7 @@ class InvoiceEletronic(models.Model):
         pag = [{
             'indPag': self.payment_term_id.indPag or '0',
             'tPag': self.payment_mode_id.tipo_pagamento or '90',
-            'vPag': "%.02f" % self.valor_final
+            'vPag': '0.00',
         }]
         if self.informacoes_complementares:
             self.informacoes_complementares = self.informacoes_complementares.\
@@ -667,6 +667,7 @@ class InvoiceEletronic(models.Model):
         }
         if len(duplicatas) > 0:
             vals['cobr'] = cobr
+            pag['vPag'] = "%.02f" % self.valor_final
         return vals
 
     @api.multi
