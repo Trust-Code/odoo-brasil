681aaa3445d4554a57804e7df3c15bd72edcb11d
marinaGD <36993009+marinaGD@users.noreply.github.com>
2018-09-25 16:12:09 -0300

    fix string of 'tax_inss_id' (#577)

--- br_account/models/account_invoice_line.py
+++ br_account/models/account_invoice_line.py
@@ -478,7 +478,7 @@ class AccountInvoiceLine(models.Model):
     # Impostos de serviço - INSS
     # =========================================================================
     inss_rule_id = fields.Many2one('account.fiscal.position.tax.rule', 'Regra')
-    tax_inss_id = fields.Many2one('account.tax', string=u"Alíquota IRRF",
+    tax_inss_id = fields.Many2one('account.tax', string=u"Alíquota INSS",
                                   domain=[('domain', '=', 'inss')])
     inss_base_calculo = fields.Float(
         u'Base INSS', required=True, digits=dp.get_precision('Account'),
