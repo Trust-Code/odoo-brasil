f98d3612ad90b337f2ba22cc0416486612f324ba
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-10-03 19:22:54 -0300

    [FIX] Fix the call to generate danfce

--- br_nfe/reports/danfe_report.py
+++ br_nfe/reports/danfe_report.py
@@ -57,10 +57,11 @@ class IrActionsReport(models.Model):
         timezone = pytz.timezone(self.env.context.get('tz')) or pytz.utc
 
         xml_element = etree.fromstring(nfe_xml)
-        obj_danfe = danfe
         if nfe.model == '65':
-            obj_danfe = danfce
-        oDanfe = obj_danfe(list_xml=[xml_element], logo=tmpLogo,
+            oDanfe = danfce(
+                list_xml=[xml_element], logo=tmpLogo, timezone=timezone)
+        else:
+            oDanfe = danfe(list_xml=[xml_element], logo=tmpLogo,
                            cce_xml=cce_xml_element, timezone=timezone)
 
         tmpDanfe = BytesIO()
