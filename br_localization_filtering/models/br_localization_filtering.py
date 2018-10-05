a991de885bc50809b5c5155739422b0763a43328
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-10-05 18:39:04 -0300

    [FIX] Add the two options for the chart template and enable to configure a different chart account in parameters

--- br_localization_filtering/models/br_localization_filtering.py
+++ br_localization_filtering/models/br_localization_filtering.py
@@ -9,27 +9,33 @@ from odoo import api, fields, models
 class BrLocalizationFiltering(models.AbstractModel):
     _name = 'br.localization.filtering'
 
-    # TODO add br_coa_simple
     def _get_br_localization_template(self):
-        localization_template = self.env['ir.model.data'].xmlid_to_object(
+        # We can configure in parameters if the user has a diffente tmpl id
+        tmpl_id = self.env['ir.config_parameter'].sudo().get_param(
+            'l10n_br.localization.template.id', default=False)
+        tmpl_id2 = self.env['ir.model.data'].xmlid_to_object(
             'br_coa.br_account_chart_template', False)
-        return localization_template and localization_template.id or False
+        tmpl_id3 = self.env['ir.model.data'].xmlid_to_object(
+            'br_coa_simple.br_account_chart_template', False)
+        return [x for x in [
+            tmpl_id,
+            tmpl_id2 and tmpl_id2.id or False,
+            tmpl_id3 and tmpl_id3.id or False] if x]
 
     def _get_user_localization(self):
         user_localization = self.env.user.company_id.chart_template_id
         return user_localization and user_localization.id or False
 
-    def _is_user_localization(self):
-        return (self._get_br_localization_template() ==
-                self._get_user_localization())
+    def _is_user_localization(self, user_tmpl_id):
+        tmpl_ids = self._get_br_localization_template()
+        return (user_tmpl_id in tmpl_ids)
 
-    # TODO add invisible attribute to l10n_br_localization field
     @staticmethod
     def _add_localization_field(doc):
         elem = etree.Element(
             'field', {
                 'name': 'l10n_br_localization',
-                # 'invisible': '1',
+                'modifiers': '{"invisible":true, "column_invisible":true}',
             })
         nodes = doc.xpath("//tree//field") or doc.xpath("//form//field")
         if len(nodes):
@@ -58,7 +64,9 @@ class BrLocalizationFiltering(models.AbstractModel):
                              or 'invisible')
                 modifiers = json.loads(node.get("modifiers"))
                 if view_type == 'tree':
-                    modifiers[mod_field] = not self._is_user_localization()
+                    user_tmpl_id = self._get_user_localization()
+                    modifiers[mod_field] = not self._is_user_localization(
+                        user_tmpl_id)
                 if view_type == 'form':
                     domain = modifiers.get(mod_field, [])
                     if isinstance(domain, bool):
@@ -75,7 +83,8 @@ class BrLocalizationFiltering(models.AbstractModel):
         if not hasattr(self.env.user.company_id, 'chart_template_id'):
             return True
 
-        return self._is_user_localization()
+        user_tmpl_id = self._get_user_localization()
+        return self._is_user_localization(user_tmpl_id)
 
     @api.multi
     @api.depends()
@@ -86,8 +95,9 @@ class BrLocalizationFiltering(models.AbstractModel):
                 user_template = (record.company_id.chart_template_id
                                  and record.company_id.chart_template_id.id
                                  or user_template)
-            br_template = self._get_br_localization_template()
-            record.l10n_br_localization = br_template == user_template
+
+            record.l10n_br_localization = self._is_user_localization(
+                user_template)
 
     l10n_br_localization = fields.Boolean(
         compute=_compute_is_br_localization,
