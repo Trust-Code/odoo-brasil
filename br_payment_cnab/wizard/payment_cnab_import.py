15a7fe3d3df123426ec4f8bca8cdcd387aca8db2
marinaGD <mgd.marinadomingues@gmail.com>
2018-11-12 14:10:54 -0200

    add itau feedback processing

--- br_payment_cnab/wizard/payment_cnab_import.py
+++ br_payment_cnab/wizard/payment_cnab_import.py
@@ -26,13 +26,20 @@ class l10nBrPaymentCnabImport(models.TransientModel):
         cnab.load_return_file(stream)
         return cnab.header.cedente_conta, cnab.header.cedente_agencia
 
+    def _get_message(self, bank, ocorrencias):
+        msgs = get_return_message(bank, ocorrencias[0:2])
+        for msg in range(4, int(len(ocorrencias))+1, 2):
+            msgs += ' -- ' + get_return_message(bank, ocorrencias[msg-2:msg])
+        return msgs
+
     def do_import(self, cnab_file):
         stream = StringIO(cnab_file.decode('ascii'))
 
         bank = get_bank(self.journal_id.bank_id.bic)
+        acc, ag = self._get_account(cnab_file)
         loaded_cnab = File(bank)
         loaded_cnab.load_return_file(stream)
-        self.validate_journal(loaded_cnab)
+        self.validate_journal(acc, ag)
 
         statement = self.env['l10n_br.payment.statement'].create({
             'journal_id': self.journal_id.id,
@@ -49,23 +56,26 @@ class l10nBrPaymentCnabImport(models.TransientModel):
                 if not payment_line:
                     continue
                 cnab_code = event.ocorrencias_retorno[:2]
-                message = get_return_message(
+                message = self._get_message(
                     self.journal_id.bank_id.bic,
-                    event.ocorrencias_retorno.strip()[:2])
-                if cnab_code == 'BD':  # Inclusão OK
-                    payment_line.mark_order_line_processed(
-                        cnab_code, message
-                    )
-                elif cnab_code in ('01', '03'):  # Débito
-                    payment_line.mark_order_line_paid(
-                        cnab_code, message, statement_id=statement
-                    )
-                else:
-                    payment_line.mark_order_line_processed(
-                        cnab_code, message, rejected=True,
-                        statement_id=statement,
-                    )
+                    event.ocorrencias_retorno.strip())
+                self.select_routing(
+                    payment_line, cnab_code, bank, message, statement)
 
         action = self.env.ref(
             'br_account_payment.action_payment_statement_tree')
         return action.read()[0]
+
+    def select_routing(self, pay_line, cnab_code, bank, message, statement):
+        if cnab_code == 'BD':  # Inclusão OK
+            pay_line.mark_order_line_processed(
+                cnab_code, message
+            )
+        elif cnab_code in ('00', '03'):  # Débito
+            pay_line.mark_order_line_paid(
+                cnab_code, message, statement_id=statement
+            )
+        else:
+            pay_line.mark_order_line_processed(
+                cnab_code, message, rejected=True,
+                statement_id=statement)
