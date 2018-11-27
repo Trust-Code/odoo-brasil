92c60b87f1442aa54a5c43900aa55e53d9f528d5
marinaGD <mgd.marinadomingues@gmail.com>
2018-11-27 12:04:37 -0200

    fix codigo_convenio name

--- br_payment_cnab/serialize/cnab240.py
+++ br_payment_cnab/serialize/cnab240.py
@@ -66,9 +66,7 @@ class Cnab_240(object):
                 self._order.company_id.cnpj_cpf),
             # Usado pelo Banco para identificar o contrato - númerodo banco(4),
             # códigode agência(4 "sem DV"), número do convênio(12).
-            'codigo_convenio': bank.codigo_convenio,
-            # Para ordem de pagamento, saque em uma agência -número da agência,
-            # caso contrário preencher com zeros.
+            'codigo_convenio': bank.l10n_br_convenio_pagamento,
             'cedente_agencia': self._string_to_num(bank.bra_number, 0),
             'cedente_agencia_dv': bank.bra_number_dig,
             'cedente_conta': self._string_to_num(bank.acc_number),
