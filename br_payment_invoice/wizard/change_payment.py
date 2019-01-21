86af0377ffd976b025973554cebdb381bb5e02fb
Danimar Ribeiro <danimaribeiro@gmail.com>
2019-01-21 14:12:22 -0200

    [IMP] Ajuste na origem de multiplos pagamentos Validação para linhas já agendadas

--- br_payment_invoice/wizard/change_payment.py
+++ br_payment_invoice/wizard/change_payment.py
@@ -1,6 +1,5 @@
 
 import re
-import ast
 import logging
 from odoo import api, fields, models
 from odoo.exceptions import UserError
@@ -31,15 +30,12 @@ class WizardChangePayment(models.TransientModel):
     date_maturity = fields.Date(string="Data de Vencimento")
     discount = fields.Float(string="Desconto")
     amount = fields.Float(string="Valor a pagar")
-    line_ids = fields.Char(string="Line IDs")
 
     @api.model
     def default_get(self, fields):
         res = super(WizardChangePayment, self).default_get(fields)
-        model = self.env.context.get('active_model', [])
         active_ids = self.env.context.get('active_ids', [])
-        line_ids = self.env[model].browse(active_ids)
-        res['line_ids'] = active_ids
+        line_ids = self.env['account.move.line'].browse(active_ids)
         partner_ids = line_ids.mapped('partner_id')
         if len(partner_ids) > 1:
             raise UserError(
@@ -88,51 +84,56 @@ class WizardChangePayment(models.TransientModel):
         return False, False
 
     def action_update_info(self):
-        ids = ast.literal_eval(self.line_ids)
-        lines = self.env[self.model].browse(ids)
-        self.action_move_line(lines)
+        active_ids = self.env.context.get('active_ids', [])
+        move_lines = self.env['account.move.line'].browse(active_ids)
 
-    def action_move_line(self, move_lines):
-        linha_digitavel, barcode = self.get_barcode_information()
-        order_lines = self.env['payment.order.line'].search(
-            [('move_line_id', '=', move_lines.id)]) # agora são varias
+        if len(move_lines) > 1:
+            if not all([not x.l10n_br_order_line_id for x in move_lines]):
+                raise UserError('Esta linha já possui pagamento agendado!')
 
-        if order_lines and all(order_lines.state == 'draft'):
-            for line in order_lines:
-                line.write({
-                    'payment_mode_id': self.payment_mode_id.id,
-                    'linha_digitavel': linha_digitavel or '',
-                    'bank_account_id': self.bank_account_id.id,
-                    'discount_value': self.discount,
-                    'date_maturity': self.date_maturity or line.date_maturity
-                })
-                self.move_line_id.write({
-                    'payment_mode_id': self.payment_mode_id.id,
-                    'date_maturity':
-                        self.date_maturity or self.move_line_id.date_maturity,
-                })
-        elif order_lines:
+        order_line = False
+        if len(move_lines) == 1 and move_lines[0].l10n_br_order_line_id:
+            order_line = move_lines[0].l10n_br_order_line_id
+
+        if order_line and order_line.state == 'draft':
+            order_line.write({
+                'payment_mode_id': self.payment_mode_id.id,
+                'linha_digitavel': linha_digitavel or '',
+                'bank_account_id': self.bank_account_id.id,
+                'discount_value': self.discount,
+                'date_maturity': self.date_maturity or order_line.date_maturity
+            })
+            self.move_line_id.write({
+                'payment_mode_id': self.payment_mode_id.id,
+                'date_maturity':
+                    self.date_maturity or self.move_line_id.date_maturity,
+            })
+        elif order_line:
             raise UserError(
                 'Algum pagamento já foi processado! \
                 Não é possível processa-lo novamente!')
         else:
-            self.prepare_lines_from_invoice(
-                barcode=barcode or False, linha_digitavel=linha_digitavel)
+            vals = self._prepare_vals(move_lines)
+            order_line = self.env['payment.order.line'].\
+                action_generate_payment_order_line(self.payment_mode_id, vals)
+            move_lines.write({'l10n_br_order_line_id': order_line.id})
 
-    def prepare_lines_from_invoice(self, barcode=False, linha_digitavel=False):
-        invoice = self.move_line_id.invoice_id
-        vals = invoice.prepare_payment_line_vals(self.move_line_id)
-        vals['date_maturity'] = \
-            self.date_maturity or self.move_line_id.date_maturity
-        vals['bank_account_id'] = self.bank_account_id.id
-        vals['discount_value'] = self.discount
-        self.env['payment.order.line'].action_generate_payment_order_line(
-            self.payment_mode_id, vals)
-        if self.payment_type in ['03', '04']:
-            vals['linha_digitavel'] = linha_digitavel
-            vals['barcode'] = barcode
-        if self.date_maturity:
-            self.move_line_id.write({
-                'payment_mode_id': self.payment_mode_id.id,
-                'date_maturity': self.date_maturity,
-            })
+    def _prepare_vals(self, move_line_ids):
+        linha_digitavel, barcode = self.get_barcode_information()
+        vals = {
+            'partner_id': self.partner_id.id,
+            'amount_total': self.amount,
+            'name': ', '.join([x.name for x in move_line_ids]),
+            'partner_ref': ','.join([x.move_id.name for x in move_line_ids]),
+            'bank_account_id': self.bank_account_id.id,
+            'partner_acc_number': self.bank_account_id.acc_number,
+            'partner_bra_number': self.bank_account_id.bra_number,
+            'date_maturity': self.date_maturity,
+            'discount_value': self.discount,
+            'linha_digitavel': linha_digitavel,
+            'barcode': barcode,
+            'payment_mode_id': self.payment_mode_id.id,
+        }
+        if len(move_line_ids) == 1:
+            vals['move_line_id'] = move_line_ids[0].id,
+        return vals
