751f7a92cf99468a407b14fe4dc8d24f3aaa74cd
Danimar Ribeiro <danimaribeiro@gmail.com>
2019-04-25 12:38:59 -0300

    [WIP] Implementando nova versão da nota de petrópolis

--- br_nfse_imperial/models/account_invoice.py
+++ br_nfse_imperial/models/account_invoice.py
@@ -7,6 +7,13 @@ from odoo import models
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
+    def _prepare_edoc_vals(self, inv, inv_lines, serie_id):
+        res = super(AccountInvoice, self)._prepare_edoc_vals(
+            inv, inv_lines, serie_id)
+
+        res['ambiente'] = inv.company_id.tipo_ambiente_nfse
+        return res
+
     def _return_pdf_invoice(self, doc):
         if doc.model == '010':
             return 'br_nfse_imperial.report_br_nfse_danfe_imperial'  # Imperial
