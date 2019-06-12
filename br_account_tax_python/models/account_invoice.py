8f0f6477b365c2ce21ef322059eb5a40dffbd2d4
marinagd <mgd.marinadomingues@gmail.com>
2019-06-12 13:17:53 -0300

    [IMP] remove account.invoice copy from br_stock_account

--- br_account_tax_python/models/account_invoice.py
+++ br_account_tax_python/models/account_invoice.py
@@ -9,8 +9,6 @@ class AccountInvoiceLine(models.Model):
 
     def _prepare_tax_context(self):
         res = super(AccountInvoiceLine, self)._prepare_tax_context()
-        lines = self.invoice_id.invoice_line_ids
-        amount_total = sum(l.valor_bruto + l.price_tax - l.valor_desconto for l in lines)
         amount = (self.invoice_id.amount_total
                   + self.quantity * self.price_unit)
         res['invoice_amount'] = amount
