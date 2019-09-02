cd886ef256e159dd718ed67f8204a65ac9e2353e
marinagd <mgd.marinadomingues@gmail.com>
2019-09-02 15:20:16 -0300

    [FIX] Travis

--- br_account_payment/models/account_payment.py
+++ br_account_payment/models/account_payment.py
@@ -68,24 +68,38 @@ to process a single invoice's payment."))
 
 
     def _create_payment_entry(self, amount):
-        """ Create a journal entry corresponding to a payment, if the payment references invoice(s) they are reconciled.
+        """ Create a journal entry corresponding to a 
+            payment, if the payment references invoice(s)
+            they are reconciled.
             Return the journal entry.
         """
-        aml_obj = self.env['account.move.line'].with_context(check_move_validity=False)
-        debit, credit, amount_currency, currency_id = aml_obj.with_context(date=self.payment_date)._compute_amount_fields(amount, self.currency_id, self.company_id.currency_id)
+        aml_obj = self.env['account.move.line'].with_context(
+            check_move_validity=False)
+        debit, credit, amount_currency, currency_id = aml_obj.with_context(
+            date=self.payment_date)._compute_amount_fields(
+            amount, self.currency_id, self.company_id.currency_id)
 
         move = self.env['account.move'].create(self._get_move_vals())
 
-        #Write line corresponding to invoice payment
-        counterpart_aml_dict = self._get_shared_move_line_vals(debit, credit, amount_currency, move.id, False)
-        counterpart_aml_dict.update(self._get_counterpart_move_line_vals(self.invoice_ids))
+        # Write line corresponding to invoice payment
+        counterpart_aml_dict = self._get_shared_move_line_vals(
+            debit, credit, amount_currency, move.id, False)
+        counterpart_aml_dict.update(
+            self._get_counterpart_move_line_vals(self.invoice_ids))
         counterpart_aml_dict.update({'currency_id': currency_id})
         counterpart_aml = aml_obj.create(counterpart_aml_dict)
 
-        #Reconcile with the invoices
-        if self.payment_difference_handling == 'reconcile' and self.payment_difference:
-            writeoff_line = self._get_shared_move_line_vals(0, 0, 0, move.id, False)
-            debit_wo, credit_wo, amount_currency_wo, currency_id = aml_obj.with_context(date=self.payment_date)._compute_amount_fields(self.payment_difference, self.currency_id, self.company_id.currency_id)
+        # Reconcile with the invoices
+        if self.payment_difference_handling == 'reconcile' \
+                and self.payment_difference:
+            writeoff_line = self._get_shared_move_line_vals(
+                0, 0, 0, move.id, False)
+            debit_wo, credit_wo, amount_currency_wo, currency_id = \
+                aml_obj.with_context(
+                    date=self.payment_date)._compute_amount_fields(
+                    self.payment_difference, 
+                    self.currency_id,
+                    self.company_id.currency_id)
             writeoff_line['name'] = self.writeoff_label
             writeoff_line['account_id'] = self.writeoff_account_id.id
             writeoff_line['debit'] = debit_wo
@@ -93,28 +107,35 @@ to process a single invoice's payment."))
             writeoff_line['amount_currency'] = amount_currency_wo
             writeoff_line['currency_id'] = currency_id
             writeoff_line = aml_obj.create(writeoff_line)
-            if counterpart_aml['debit'] or (writeoff_line['credit'] and not counterpart_aml['credit']):
+            if counterpart_aml['debit'] or (
+                    writeoff_line['credit'] and not \
+                    counterpart_aml['credit']):
                 counterpart_aml['debit'] += credit_wo - debit_wo
-            if counterpart_aml['credit'] or (writeoff_line['debit'] and not counterpart_aml['debit']):
+            if counterpart_aml['credit'] or (
+                    writeoff_line['debit'] and not counterpart_aml['debit']):
                 counterpart_aml['credit'] += debit_wo - credit_wo
             counterpart_aml['amount_currency'] -= amount_currency_wo
 
-        #Write counterpart lines
+        # Write counterpart lines
         if not self.currency_id.is_zero(self.amount):
             if not self.currency_id != self.company_id.currency_id:
                 amount_currency = 0
-            liquidity_aml_dict = self._get_shared_move_line_vals(credit, debit, -amount_currency, move.id, False)
-            liquidity_aml_dict.update(self._get_liquidity_move_line_vals(-amount))
+            liquidity_aml_dict = self._get_shared_move_line_vals(
+                credit, debit, -amount_currency, move.id, False)
+            liquidity_aml_dict.update(
+                self._get_liquidity_move_line_vals(-amount))
             aml_obj.create(liquidity_aml_dict)
 
-        #validate the payment
+        # validate the payment
         if not self.journal_id.post_at_bank_rec:
             move.post()
 
-        #reconcile the invoice receivable/payable line(s) with the payment
+        # reconcile the invoice receivable/payable line(s) with the payment
         if self.invoice_ids:
             if self.move_line_id:
-                self.invoice_ids.with_context(move_line_to_reconcile=self.move_line_id).register_payment(counterpart_aml)
+                self.invoice_ids.with_context(
+                    move_line_to_reconcile=self.move_line_id).\
+                        register_payment(counterpart_aml)
             else:
                 self.invoice_ids.register_payment(counterpart_aml)
         return move
