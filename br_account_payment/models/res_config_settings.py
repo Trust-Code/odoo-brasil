f3233ffe12ed6d7b639b984ff970c9b033a71e35
Gregório Stockler <gregoriostockler1@gmail.com>
2019-10-04 08:57:42 -0300

    create getters and setter to account payment config filds

--- br_account_payment/models/res_config_settings.py
+++ br_account_payment/models/res_config_settings.py
@@ -1,30 +1,64 @@
 # © 2018 Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     l10n_br_payment_interest_account_id = fields.Many2one(
-        'account.account',
+        "account.account",
         string="Conta para pagamento de juros/multa",
-        help='Conta onde será registrado o montante dos juros e multas pagos'
+        help="Conta onde será registrado o montante dos juros e multas pagos",
     )
     l10n_br_payment_discount_account_id = fields.Many2one(
-        'account.account',
+        "account.account",
         string="Conta para desconto de pagamentos",
-        help='Conta onde será registrado o desconto recebido de pagamentos'
+        help="Conta onde será registrado o desconto recebido de pagamentos",
     )
 
     l10n_br_interest_account_id = fields.Many2one(
-        'account.account',
+        "account.account",
         string="Conta para recebimento de juros",
-        help='Conta onde será creditado o montante dos juros recebidos'
+        help="Conta onde será creditado o montante dos juros recebidos",
     )
     l10n_br_bankfee_account_id = fields.Many2one(
-        'account.account',
+        "account.account",
         string="Conta para tarifas bancárias",
-        help='Conta onde será debitado o montante de tarifas pagas'
+        help="Conta onde será debitado o montante de tarifas pagas",
     )
+
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        res[
+            "l10n_br_payment_interest_account_id"
+        ] = self.env.user.company_id.l10n_br_payment_interest_account_id.id
+        res[
+            "l10n_br_payment_discount_account_id"
+        ] = self.env.user.company_id.l10n_br_payment_discount_account_id.id
+        res[
+            "l10n_br_interest_account_id"
+        ] = self.env.user.company_id.l10n_br_interest_account_id.id
+        res[
+            "l10n_br_bankfee_account_id"
+        ] = self.env.user.company_id.l10n_br_bankfee_account_id.id
+        return res
+
+    @api.multi
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        self.env.user.company_id.l10n_br_payment_interest_account_id = (
+            self.l10n_br_payment_interest_account_id
+        )
+        self.env.user.company_id.l10n_br_payment_discount_account_id = (
+            self.l10n_br_payment_discount_account_id
+        )
+        self.env.user.company_id.l10n_br_interest_account_id = (
+            self.l10n_br_interest_account_id
+        )
+        self.env.user.company_id.l10n_br_bankfee_account_id = (
+            self.l10n_br_bankfee_account_id
+        )
+
