a4a52317807d1818a018934f71b98ead90cd8fd7
marcelsavegnago <marcel.savegnago@gmail.com>
2019-11-11 14:36:19 -0300

    [FIX] Correção dos campos de juros, tarifas e descontos na configuração da contabilidade.

--- br_account_payment/models/res_config_settings.py
+++ br_account_payment/models/res_config_settings.py
@@ -10,21 +10,28 @@ class ResConfigSettings(models.TransientModel):
     l10n_br_payment_interest_account_id = fields.Many2one(
         'account.account',
         string="Conta para pagamento de juros/multa",
-        help='Conta onde será registrado o montante dos juros e multas pagos'
+        help='Conta onde será registrado o montante dos juros e multas pagos',
+        related='company_id.l10n_br_payment_interest_account_id',
+        readonly=False
     )
     l10n_br_payment_discount_account_id = fields.Many2one(
         'account.account',
         string="Conta para desconto de pagamentos",
-        help='Conta onde será registrado o desconto recebido de pagamentos'
+        help='Conta onde será registrado o desconto recebido de pagamentos',
+        related='company_id.l10n_br_payment_discount_account_id',
+        readonly=False
     )
-
     l10n_br_interest_account_id = fields.Many2one(
         'account.account',
         string="Conta para recebimento de juros",
-        help='Conta onde será creditado o montante dos juros recebidos'
+        help='Conta onde será creditado o montante dos juros recebidos',
+        related='company_id.l10n_br_interest_account_id',
+        readonly=False
     )
     l10n_br_bankfee_account_id = fields.Many2one(
         'account.account',
         string="Conta para tarifas bancárias",
-        help='Conta onde será debitado o montante de tarifas pagas'
+        help='Conta onde será debitado o montante de tarifas pagas',
+        related='company_id.l10n_br_bankfee_account_id',
+        readonly=False
     )
