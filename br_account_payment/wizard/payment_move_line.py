6857bfba75e30e3426cd6844b7e6b85fa83cb1d6
Danimar Ribeiro <danimaribeiro@gmail.com>
2020-02-21 11:58:50 -0300

    Remove unused import

--- br_account_payment/wizard/payment_move_line.py
+++ br_account_payment/wizard/payment_move_line.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models, api, _
-from odoo.exceptions import ValidationError, UserError
+from odoo.exceptions import UserError
 
 
 class PaymentAccountMoveLine(models.TransientModel):
