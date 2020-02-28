5a537a57f98f457136db1cb2280b55ea91b7fbbc
Danimar Ribeiro <danimaribeiro@gmail.com>
2020-02-28 00:20:12 -0300

    Fix domain FCP and difal taxes

--- br_account/models/account_invoice.py
+++ br_account/models/account_invoice.py
@@ -331,11 +331,12 @@ class AccountInvoice(models.Model):
         for line in self.invoice_line_ids:
             other_taxes = line.invoice_line_tax_ids.filtered(
                 lambda x: not x.domain)
-            line.invoice_line_tax_ids = other_taxes | line.tax_icms_id | \
-                line.tax_ipi_id | line.tax_pis_id | line.tax_cofins_id | \
-                line.tax_issqn_id | line.tax_ii_id | line.tax_icms_st_id | \
-                line.tax_csll_id | line.tax_irrf_id | \
-                line.tax_inss_id
+            line.invoice_line_tax_ids = line.tax_icms_id | line.tax_icms_st_id | \
+                line.tax_icms_inter_id | line.tax_icms_intra_id | \
+                line.tax_icms_fcp_id | line.tax_ipi_id | \
+                line.tax_pis_id | line.tax_cofins_id | line.tax_issqn_id | \
+                line.tax_ii_id | line.tax_csll_id | line.tax_irrf_id | \
+                line.tax_inss_id | other_taxes
 
             ctx = line._prepare_tax_context()
             tax_ids = line.invoice_line_tax_ids.with_context(**ctx)
