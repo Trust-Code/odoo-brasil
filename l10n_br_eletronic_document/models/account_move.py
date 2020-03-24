7cfc391c5e11f6be7f65f9a98a621da6c2658a7c
Danimar Ribeiro <danimar.ribeiro@nextroll.com>
2020-03-24 02:13:34 -0300

    Configurando geracao de notas fiscais apartir da fatura

--- l10n_br_eletronic_document/models/account_move.py
+++ l10n_br_eletronic_document/models/account_move.py
@@ -10,14 +10,59 @@ class AccountMove(models.Model):
          ('after_payment', 'Emitir após pagamento'),
          ('manually', 'Manualmente')], string="Nota Eletrônica", default='directly')
 
+    def _prepare_eletronic_doc_vals(self):
+        return {
+            'name': self.name,
+            'company_id': self.company_id.id,
+            'partner_id': self.partner_id.id,
+            'move_id': self.id,
+            'schedule_user_id': self.env.user.id,
+            'state': 'draft',
+            'tipo_operacao': 'saida',
+            'data_agendada': self.invoice_date,
+            'finalidade_emissao': '1',
+            'partner_id': self.partner_id.id,
+            'payment_term_id': self.invoice_payment_term_id.id,
+            'fiscal_position_id': self.fiscal_position_id.id,
+            'valor_final': self.amount_total,
+        }
+
+    def _prepare_eletronic_line_vals(self, lines):
+        result = []
+        for line in lines:
+            result += [(0, 0, {
+                'name': line.name,
+                'product_id': line.product_id.id,
+                #'tipo_produto': line.product_type,
+                #'cfop': line.cfop_id.code,
+                'uom_id': line.product_uom_id.id,
+                'quantidade': line.quantity,
+                'preco_unitario': line.price_unit,
+                #'valor_bruto': line.valor_bruto,
+                #'desconto': line.valor_desconto,
+                'valor_liquido': line.price_subtotal,
+                #'origem': line.icms_origem,
+                #'tributos_estimados': line.tributos_estimados,
+                #'ncm': line.fiscal_classification_id.code,
+                #'pedido_compra': line.pedido_compra,
+                #'item_pedido_compra': line.item_pedido_compra,
+            })]
+        return result
+
+
     def action_create_eletronic_document(self):
         for move in self:
-            self.env['eletronic.document'].create({
-                'name': move.name,
-                'company_id': move.company_id.id,
-                'partner_id': move.partner_id.id,
-                'move_id': move.id,
-            })
+            vals = move._prepare_eletronic_doc_vals()
+            services = move.invoice_line_ids.filtered(lambda  x: x.product_id.type == 'service')
+            if services:
+                vals['model'] = 'nfse'
+                vals['document_line_ids'] = self._prepare_eletronic_line_vals(services)
+                self.env['eletronic.document'].create(vals)
+            products = move.invoice_line_ids.filtered(lambda  x: x.product_id.type != 'service')
+            if products:
+                vals['model'] = 'nfe'
+                vals['document_line_ids'] = self._prepare_eletronic_line_vals(products)
+                self.env['eletronic.document'].create(vals)
 
     def action_post(self):
         res = super(AccountMove, self).action_post()
--- l10n_br_eletronic_document/models/eletronic_document.py
+++ l10n_br_eletronic_document/models/eletronic_document.py
@@ -121,14 +121,9 @@ class EletronicDocument(models.Model):
          ('saida', 'Saída')],
         string=u'Tipo de Operação', readonly=True, states=STATE)
     model = fields.Selection(
-        [('55', u'55 - NFe'),
-         ('65', u'65 - NFCe'),
-         ('001', u'NFS-e - Nota Fiscal Paulistana'),
-         ('002', u'NFS-e - Provedor GINFES'),
-         ('008', u'NFS-e - Provedor SIMPLISS'),
-         ('009', u'NFS-e - Provedor SUSESU'),
-         ('010', u'NFS-e Imperial - Petrópolis'),
-         ('012', u'NFS-e - Florianópolis')],
+        [('nfe', '55 - NFe'),
+         ('nfce', '65 - NFCe'),
+         ('nfse', 'NFS-e - Nota Fiscal de Servico')],
         string=u'Modelo', readonly=True, states=STATE)
     # serie = fields.Many2one(
     #     'br_account.document.serie', string=u'Série',
