8843fff82136f5205f72a760d425cb2e174567e0
Danimar Ribeiro <danimar.ribeiro@nextroll.com>
2020-04-06 01:43:29 -0300

    Relatórios Mis Builder - DRE

---
+++ l10n_br_account_reports/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Account Reports',
+    'version': '13.0.1.0.0',
+    'category': 'Account',
+    'author': 'Trustcode',
+    'license': 'OPL-1',
+    'summary': "Create some essential account reports",
+    'website': 'https://www.odoo-next.com.br',
+    'support': 'comercial@trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>'
+    ],
+    'depends': [
+        'mis_builder',
+    ],
+    'data': [
+        'data/styles.xml',
+        'data/dre.xml',
+    ],
+}
