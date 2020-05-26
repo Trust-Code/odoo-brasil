e5d6380b3bf8d9d5b2885828f1caee1cf12a76bc
Danimar Ribeiro <danimar.ribeiro@nextroll.com>
2020-05-26 12:12:27 -0300

    Criado tambem o lancamento de taxas do IUGU

--- l10n_br_automated_payment/models/payment_transaction.py
+++ l10n_br_automated_payment/models/payment_transaction.py
@@ -25,6 +25,7 @@ class PaymentTransaction(models.Model):
             if data.get('status', '') == 'paid' and self.state not in ('done', 'authorized'):
                 self._set_transaction_done()
                 self._post_process_after_done()
+                self.origin_move_line_id._create_bank_tax_move(data)
             else:
                 self.iugu_status = data['status']
         else:
