ef842364e7edfb8cbc7002c77a41f8f0b65e6ca8
Carlos R. Silveira <crsilveira@gmail.com>
2020-05-26 17:04:39 -0300

    Corrigindo arquivo.

--- br_sped_efd_icms_ipi/__init__.py
+++ br_sped_efd_icms_ipi/__init__.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# Copyright (C) 2020 - Carlos R. Silveira - ATSti Soluções
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
