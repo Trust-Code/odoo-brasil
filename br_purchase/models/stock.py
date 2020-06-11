47495a1b0a335a2e7fb4a679bcc384b2075c39d3
Danimar Ribeiro <danimaribeiro@gmail.com>
2020-06-10 22:14:27 -0300

    We used the price from the move that were previously calculated in confirming the order

--- br_purchase/models/stock.py
+++ br_purchase/models/stock.py
@@ -13,28 +13,7 @@ class StockMove(models.Model):
         self.ensure_one()
         if self.purchase_line_id \
                 and self.product_id.id == self.purchase_line_id.product_id.id:
-            line = self.purchase_line_id
-            order = line.order_id
-            ctx = line._prepare_tax_context()
-            tax_ids = line.taxes_id.with_context(**ctx)
-            price = line.price_unit
-            if line.taxes_id:
-                taxes = tax_ids.compute_all(
-                    price,
-                    currency=line.order_id.currency_id,
-                    quantity=1.0,
-                    product=self.product_id,
-                    partner=line.order_id.partner_id)
 
-                price = taxes['total_included']
-                for tax in taxes['taxes']:
-                    if tax['account_id']:
-                        price -= tax['amount']
-            if line.product_uom.id != line.product_id.uom_id.id:
-                price *= line.product_uom.factor/line.product_id.uom_id.factor
-            if order.currency_id != order.company_id.currency_id:
-                price = order.currency_id.compute(price,
-                                                  order.company_id.currency_id,
-                                                  round=False)
-            return price
+            return self.price_unit or self.product_id.standard_price
+
         return super(StockMove, self)._get_price_unit()
