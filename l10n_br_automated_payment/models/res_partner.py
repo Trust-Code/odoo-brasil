15d9b90fb42be9aca543a213c4d7d5da6098ed6a
Johny Chen Jy <johnychenjy@gmail.com>
2020-10-22 11:47:46 -0300

    update cpf and error iteration

--- l10n_br_automated_payment/models/res_partner.py
+++ l10n_br_automated_payment/models/res_partner.py
@@ -24,7 +24,7 @@ class ResPartner(models.Model):
                 'email': partner.email,
                 'name': commercial_part.l10n_br_legal_name or commercial_part.name,
                 'notes': commercial_part.comment or '',
-                'cpf_cnpj': commercial_part.l10n_br_cnpj_cpf,
+                'cpf_cnpj': re.sub('[^0-9]', '', commercial_part.l10n_br_cnpj_cpf or ''),
                 'zip_code': re.sub('[^0-9]', '', commercial_part.zip or ''),
                 'number': commercial_part.l10n_br_number,
                 'street': commercial_part.street,
@@ -36,10 +36,17 @@ class ResPartner(models.Model):
             if not partner.iugu_id:
                 data = iugu_customer_api.create(vals)
                 if "errors" in data:
-                    msg = "\n".join(
-                        ["A integração com IUGU retornou os seguintes erros"] +
-                        ["Field: %s %s" % (x[0], x[1][0])
-                         for x in data['errors'].items()])
+                    if isinstance(data['errors'], str):
+                        msg = "\n".join(
+                            ["A integração com IUGU retornou os seguintes erros"] +
+                            [data['errors']])
+
+                    elif isinstance(data['errors'], dict):
+                        msg = "\n".join(
+                            ["A integração com IUGU retornou os seguintes erros"] +
+                            ["Field: %s %s" % (x[0], x[1][0])
+                             for x in data['errors'].items()])
+
                     raise UserError(msg)
                 partner.iugu_id = data['id']
             else:
