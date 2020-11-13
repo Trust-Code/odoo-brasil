5335bdde323fd219cc5f59c849650c067a57b7fd
Johny Chen Jy <johnychenjy@gmail.com>
2020-11-13 17:38:56 -0300

    Tax is calculated when fpos or order_line changes

---
+++ l10n_br_purchase/__manifest__.py
@@ -0,0 +1,18 @@
+{  # pylint: disable=C8101,C8103
+    "name": "Odoo Next - Purchase",
+    "description": "Purchase Localization",
+    "version": "13.0.1.0.0",
+    "category": "Localization",
+    "author": "Code 137",
+    'license': 'Other OSI approved licence',
+    "contributors": [
+        "Johny Chen Jy <johnychenjy@gmail.com>"
+    ],
+    "depends": [
+        "purchase",
+    ],
+    'data':[
+
+    ],
+    "auto_install": True,
+}
