59c0de1f12e16b99e95f845be55b7f78ec780964
Fabio Luna <fabiocluna@hotmail.com>
2020-11-25 16:15:34 -0300

    [FIX] l10n_br_account: Account Mapping
    Fix error when mapping accounts based on fiscal position. Now the
    account mapping are undoing before the payment lines recompute and
    doing after this. It is necessary because the recompute always do
    the lines balance and as the account usually do not be a payable or
    receivable account, some unnecessary line created.

--- l10n_br_account/models/account_move.py
+++ l10n_br_account/models/account_move.py
@@ -28,26 +28,30 @@ class AccountMove(models.Model):
             for line in move_lines:
                 line.account_id = account_id
 
-    # @api.onchange("fiscal_position_id")
-    # def _onchange_fiscal_position_id(self):
-        # self._mapping_fiscal_position_account()
+    def _unmap_lines(self):
+        if not self.fiscal_position_id:
+            return
 
-    # @api.onchange("invoice_line_ids")
-    # def _onchange_invoice_line_ids(self):
-        # super(AccountMove, self)._onchange_invoice_line_ids()
-        # self._mapping_fiscal_position_account()
+        fiscal_position_id = self.fiscal_position_id
 
-    def action_post(self):
+        if fiscal_position_id.account_id:
+            if self.is_sale_document(include_receipts=True):
+                self.mapped("line_ids").filtered(
+                    lambda x: x.account_id == fiscal_position_id.account_id
+                ).account_id = self.partner_id.property_account_payable_id
+            elif self.is_purchase_document(include_receipts=True):
+                self.mapped("line_ids").filtered(
+                    lambda x: x.account_id == fiscal_position_id.account_id
+                ).account_id = self.partner_id.property_account_receivable_id
+
+    def _recompute_payment_terms_lines(self):
+        self._unmap_lines()
+        super(AccountMove, self)._recompute_payment_terms_lines()
         self._mapping_fiscal_position_account()
-        return super(AccountMove, self).action_post()
 
-    # def _recompute_dynamic_lines(
-        # self, recompute_all_taxes=False, recompute_tax_base_amount=False
-    # ):
-        # super(AccountMove, self)._recompute_dynamic_lines(
-            # recompute_all_taxes, recompute_tax_base_amount
-        # )
-
+    @api.onchange("fiscal_position_id")
+    def _onchange_fiscal_position_id(self):
+        self._mapping_fiscal_position_account()
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
@@ -83,16 +87,16 @@ class AccountMoveLine(models.Model):
         return tax_ids | self.move_id.fiscal_position_id.apply_tax_ids
 
     def action_register_payment_move_line(self):
-        dummy, act_id = self.env['ir.model.data'].get_object_reference(
-            'l10n_br_account', 'action_payment_account_move_line'
+        dummy, act_id = self.env["ir.model.data"].get_object_reference(
+            "l10n_br_account", "action_payment_account_move_line"
         )
-        receivable = (self.account_id.internal_type == 'receivable')
-        vals = self.env['ir.actions.act_window'].browse(act_id).read()[0]
-        vals['context'] = {
-            'default_amount': self.debit or self.credit,
-            'default_partner_type': 'customer' if receivable else 'supplier',
-            'default_partner_id': self.partner_id.id,
-            'default_communication': self.name,
-            'default_move_line_id': self.id,
+        receivable = self.account_id.internal_type == "receivable"
+        vals = self.env["ir.actions.act_window"].browse(act_id).read()[0]
+        vals["context"] = {
+            "default_amount": self.debit or self.credit,
+            "default_partner_type": "customer" if receivable else "supplier",
+            "default_partner_id": self.partner_id.id,
+            "default_communication": self.name,
+            "default_move_line_id": self.id,
         }
         return vals
