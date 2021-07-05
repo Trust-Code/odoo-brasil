710314c4b8508e8624dcc5920c9bb73d7ad45a15
jonatasbiazus <jonatasbiazusct@gmail.com>
2021-07-05 10:18:51 -0300

    Plano de contas

---
+++ br_coa_me_epp/models/account_tax.py
@@ -0,0 +1,28 @@
+from odoo import models, fields
+
+
+class AccountTaxTemplate(models.Model):
+    _inherit = 'account.tax.template'
+
+    domain = fields.Selection([('icms', 'ICMS'),
+                               ('icmsst', 'ICMS ST'),
+                               ('pis', 'PIS'),
+                               ('cofins', 'COFINS'),
+                               ('ipi', 'IPI'),
+                               ('iss', 'ISS'),
+                               ('ii', 'II'),
+                               ('icms_inter', 'Difal - Alíquota Inter'),
+                               ('icms_intra', 'Difal - Alíquota Intra'),
+                               ('fcp', 'FCP'),
+                               ('irpj', 'IRPJ'),
+                               ('csll', 'CSLL'),
+                               ('irrf', 'IRRF'),
+                               ('inss', 'INSS'),
+                               ('outros', 'Outros')], string="Tipo")
+
+    def _get_tax_vals(self, company, tax_template_to_tax):
+        # import pdb; pdb.set_trace()
+        res = super(AccountTaxTemplate, self)._get_tax_vals(
+            company, tax_template_to_tax)
+        res['domain'] = self.domain
+        return res
