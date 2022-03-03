18ce31528a0b17192363d6dc06fd27556347078b
Danimar Ribeiro <danimaribeiro@gmail.com>
2022-03-03 00:32:06 -0300

    Revert "[IMP] Retention"
    This reverts commit 9a2bb19b791d9bd8b45bf1748f27a1d170f74982.

--- l10n_br_account/models/account_tax.py
+++ l10n_br_account/models/account_tax.py
@@ -19,26 +19,3 @@ class AccountTax(models.Model):
                                ('irrf', 'IRRF'),
                                ('inss', 'INSS'),
                                ('outros', 'Outros')], string="Tipo")
-
-    def compute_all(self, price_unit, currency=None, quantity=1.0, product=None, partner=None, is_refund=False, handle_price_include=True, include_caba_tags=False):
-        res = super(AccountTax, self).compute_all(
-            price_unit,
-            currency,
-            quantity,
-            product,
-            partner,
-            is_refund,
-            handle_price_include,
-            include_caba_tags,
-        )
-
-        retention_taxes = list(filter(lambda x: x["amount"] < 0, res["taxes"]))
-        amount_retention = 0
-
-        for tax in retention_taxes:
-            tax["base"] += tax["amount"]
-            amount_retention += tax["amount"]
-
-        res["total_excluded"] += amount_retention
-        res["total_void"] += amount_retention
-        return res
