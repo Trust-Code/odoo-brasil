6f743923b4bd307b06833a9ef8355c7ab05037ce
Felipe Paloschi <paloschi.eca@gmail.com>
2023-01-25 15:20:41 -0300

    l10n_br_stock_account: Finish migration

--- l10n_br_stock_account/__manifest__.py
+++ l10n_br_stock_account/__manifest__.py
@@ -1,18 +1,19 @@
 {  # pylint: disable=C8101,C8103
-    'name': 'Odoo Next - ',
-    'description': 'Enable Tax Calculations',
-    'version': '14.0.1.0.0',
-    'category': 'Localization',
-    'author': 'Code 137',
-    'license': 'OEEL-1',
-    'website': 'http://www.code137.com.br',
-    'contributors': [
-        'Fábio Luna <fabiocluna@hotmail.com>',
+    "name": "Odoo Next - Stock Account",
+    "description": "",
+    "version": "15.0.1.0.0",
+    "category": "Localization",
+    "author": "Code 137",
+    "license": "OEEL-1",
+    "website": "http://www.code137.com.br",
+    "contributors": [
+        "Fábio Luna <fabiocluna@hotmail.com>",
+        "Felipe Paloschi <paloschi.eca@gmail.com>"
     ],
-    'depends': [
-        'stock_account',
+    "depends": [
+        "stock_account",
     ],
-    'data': [
-        'views/stock_picking.xml',
+    "data": [
+        "views/stock_picking.xml",
     ],
 }
